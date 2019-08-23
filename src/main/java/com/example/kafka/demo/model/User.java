c978d4debe7239c75463336bb0c8b850424b054d
JStheBest <zwang198@uiowa.edu>
2019-08-23 08:45:28 +0800

    kafka
    kafka

---
+++ src/main/java/com/example/kafka/demo/model/User.java
@@ -0,0 +1,37 @@
+package com.example.kafka.demo.model;
+
+public class User {
+    public String name;
+    public String dept;
+    public Long salary;
+
+    public User(String name, String dept, Long salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+}
